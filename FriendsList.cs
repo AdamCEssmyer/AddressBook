53794ff424f31079f4444b7408e4c72dd84f93f8
Adam Essmyer <acemyer@gmail.com>
2021-06-15 20:48:48 -0500

    created method for adding people

--- FriendsList.cs
+++ FriendsList.cs
@@ -10,6 +10,29 @@ public class Person
     public string[] Addresses {get; set;
     }
 
+    private static void AddPerson()
+    {
+        Person person = new Person();
+
+        Console.Write("Enter First Name: ");
+        person.FirstName = Console.ReadLine();
+
+        Console.Write("Enter Last Name: ");
+        person.LastName = Console.ReadLine();
+
+        Console.Write("Enter Phone Number:");
+        person.PhoneNumber = Console.ReadLine();
+
+        Console.Write("Enter Address 1: ");
+        string[] address = new string[2];
+        addresses[0] = Console.ReadLine();
+        Console.Write("Enter Address 2(Optional): ");
+        addresses[1] = Console.ReadLine();
+        person.Addresses = addresses;
+        
+        People.Add(person);
+    }
+
 
 
 
