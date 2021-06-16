8d99b7cff618691f1a377781f89a6662d2055e03
Adam Essmyer <acemyer@gmail.com>
2021-06-15 20:42:26 -0500

    first commit

---
+++ FriendsList.cs
@@ -0,0 +1,17 @@
+
+
+public static List<Person>People = new List<Person>();
+
+public class Person
+{
+    public string FirstName {get; set;}
+    public string LastName {get; set;}
+    public string PhoneNumber {get; set;}
+    public string[] Addresses {get; set;
+    }
+
+
+
+
+
+
