fd101117e3b33aff768164c2e3b561d9729dbca5
Adam Essmyer <acemyer@gmail.com>
2021-06-18 10:19:51 -0500

    created first or default method

--- FriendsList.cs
+++ FriendsList.cs
@@ -63,7 +63,23 @@ public class Person
     Console.WriteLine("Enter the first name of the person you would like to remove.");
     string firstName = Console.ReadLine();
     Person person = People.FirstOrDefault(x => x.FirstName.ToLower() == firstName.ToLower());
-    
+
+    if(person == null)
+    {
+        Console.WriteLine("That person could not be found. Press any key to continue.");
+        Console.ReadKey();
+        return;
+    }
+
+    Console.WriteLine("Are you sure you want to remove this person from your address book?(Y/N)");
+    PrintPerson(person);
+
+    if(Console.ReadKey().Key == ConsoleKey.Y)
+    {
+        People.Remove(person);
+        Console.WriteLine("Person removed. Press any key to continue.");
+        Console.ReadKey();
+    }
 
 
 
